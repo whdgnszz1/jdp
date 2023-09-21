dc78d162b67808b9edf1444a7ef65e30eb11c5cc
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-09-22 08:07:46 +0900

    Add github actions secret key

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 const router = express.Router();
 
 router.get('/', (req: Request, res: Response) => {
-  res.status(200).send('cicd');
+  res.status(200).send('github actions');
 });
 
 export default router;
